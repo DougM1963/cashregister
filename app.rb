71d07e824d68b377a3c5e464a4315856e9af614b
dougm1963 <dougmeador1963@gmail.com>
2018-01-24 09:23:40 -0500

    finished program

--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ post '/item_choice' do
     socks = params[:socks]
     underwear = params[:underwear]
     shoes = params[:shoes]
+    puts jeans, sweatshirt, socks, underwear, shoes
     redirect '/result?jeans=' + jeans +'&sweatshirt=' + sweatshirt + '&socks=' + socks +'&underwear=' + underwear + '&shoes=' + shoes
 end
 
@@ -24,21 +25,26 @@ get '/result' do
 end
 
 post '/collection' do
+    payment = params[:payment]
+    total = params[:total]
+    gtotal = params[:gtotal]
     jeans = params[:jeans]
     sweatshirt = params[:sweatshirt]
     socks = params[:socks]
     underwear = params[:underwear]
     shoes = params[:shoes]
-    payment = params[:payment]
-    redirect '/final?jeans=' + jeans + '&sweatshirt=' + sweatshirt + '&socks=' + socks +'&underwear=' + underwear + '&shoes=' + shoes + '&payment=' + payment
+    puts jeans, sweatshirt, socks, underwear, shoes, payment
+    redirect '/final?payment=' + payment + '&total=' + total + '&gtotal=' + gtotal + '&jeans=' + jeans + '&sweatshirt=' + sweatshirt + '&socks=' + socks +'&underwear=' + underwear + '&shoes=' + shoes 
 end
 
 get '/final' do
+    payment = params[:payment]  
+    total = params[:total]
+    gtotal = params[:gtotal]
 	jeans = params[:jeans]
     sweatshirt = params[:sweatshirt]
     socks = params[:socks]
     underwear = params[:underwear]
     shoes = params[:shoes]
-    payment = params[:payment]
-    erb :final, :locals => {:jeans => jeans, :sweatshirt => sweatshirt, :socks => socks, :underwear => underwear, :shoes => shoes, :payment => payment}
+    erb :final, :locals => {:payment => payment, :total => total, :gtotal => gtotal, :jeans => jeans, :sweatshirt => sweatshirt, :socks => socks, :underwear => underwear, :shoes => shoes}
 end
